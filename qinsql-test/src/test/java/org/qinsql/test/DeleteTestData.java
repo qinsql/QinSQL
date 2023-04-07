e0bd68c8bb079a03aafde7ffced743f4f6f8cd2e
Honghua Zhu <zhh200910@gmail.com>
2023-04-07 22:02:04 +0800

    删除qinsql-p2p模块

--- qinsql-test/src/test/java/org/qinsql/test/DeleteTestData.java
+++ qinsql-test/src/test/java/org/qinsql/test/DeleteTestData.java
@@ -21,13 +21,15 @@ import java.io.File;
 import java.io.IOException;
 
 import org.lealone.storage.fs.FileUtils;
+import org.lealone.test.TestBase;
 
 public class DeleteTestData {
 
     public static void main(String[] args) throws IOException {
         FileUtils.deleteRecursive(TestBase.TEST_BASE_DIR, true);
         if (!FileUtils.exists(TestBase.TEST_BASE_DIR)) {
-            System.out.println("dir '" + new File(TestBase.TEST_BASE_DIR).getCanonicalPath() + "' deleted");
+            System.out.println(
+                    "dir '" + new File(TestBase.TEST_BASE_DIR).getCanonicalPath() + "' deleted");
         }
     }
 

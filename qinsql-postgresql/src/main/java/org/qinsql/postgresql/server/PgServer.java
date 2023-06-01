105fd74abc993b3472b2a490d522d77e8518291e
Honghua Zhu <zhh200910@gmail.com>
2023-06-01 17:14:23 +0800

    包名是org.qingsql

--- qinsql-postgresql/src/main/java/org/qinsql/postgresql/server/PgServer.java
+++ qinsql-postgresql/src/main/java/org/qinsql/postgresql/server/PgServer.java
@@ -21,7 +21,7 @@ public class PgServer extends AsyncServer<PgServerConnection> {
     private static final Logger logger = LoggerFactory.getLogger(PgServer.class);
 
     public static final String PG_VERSION = "8.2.23";
-    public static final String PG_CATALOG_FILE = "/org/lealone/plugins/postgresql/resources/pg_catalog.sql";
+    public static final String PG_CATALOG_FILE = "/org/qinsql/postgresql/resources/pg_catalog.sql";
     public static final int DEFAULT_PORT = 9510;
 
     private final HashSet<Integer> typeSet = new HashSet<>();

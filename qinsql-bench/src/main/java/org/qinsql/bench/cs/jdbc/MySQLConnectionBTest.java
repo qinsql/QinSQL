d41277ff647bf7ee1d3bf891df458fbd751fdd93
Honghua Zhu <zhh200910@gmail.com>
2023-06-11 16:23:21 +0800

    add MySQLConnectionBTest

---
+++ qinsql-bench/src/main/java/org/qinsql/bench/cs/jdbc/MySQLConnectionBTest.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright Lealone Database Group.
+ * Licensed under the Server Side Public License, v 1.
+ * Initial Developer: zhh
+ */
+package org.qinsql.bench.cs.jdbc;
+
+public class MySQLConnectionBTest extends ConnectionBTest {
+
+    public static void main(String[] args) {
+        new MySQLConnectionBTest().start();
+    }
+}
